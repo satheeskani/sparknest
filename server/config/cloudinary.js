2c696ddabc0bf115edbfc7edc5faf2e909d054ff
Satheeskumar Marikani <satheeskani1995@gmail.com>
2026-03-13 16:37:17 +0530

    fix: downgrade cloudinary to v1 for multer-storage-cloudinary compat

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,11 +1,9 @@
-import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
-dotenv.config();
+import cloudinary from "cloudinary";
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key:    process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+export default cloudinary.v2;
